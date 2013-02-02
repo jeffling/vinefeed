6da1b845a9acd53f9fc482345cc84c65fa12128f
Jeff Ling <jeff.ummu@gmail.com>
2013-02-02 12:10:40 -0500

    removed mute, controlling volume through volumn instead. hover in and out work.

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 var socket = io.connect('http://localhost');
 socket.on('tweet', function (data) {
-	$("<video id='" + data.tweet.id + "' class='video-js vjs-default-skin' loop preload='auto' width=200 height=200 muted src='" + 
+	$("<video id='" + data.tweet.id + "' class='video-js vjs-default-skin' loop preload='auto' width=200 height=200 src='" + 
     data.tweet.vid_url + "''></video>").prependTo("#videos");
 
   _V_(String(data.tweet.id)).ready(function() {
@@ -8,14 +8,12 @@ socket.on('tweet', function (data) {
     this.play();
   });
   
-  // // mouseover in mouseover out callbacks
-  // $("#" + data.tweet.id).hover(function(event){
-  //   _V_(String(data.tweet.id)).volume(1);
-  //   alert("test");
-  // }, function(event) {
-  //   _V_(String(data.tweet.id)).volume(0);
-  // });
-
+  // mouseover in mouseover out callbacks
+  $("#" + data.tweet.id).hover(function(){
+    _V_(String(data.tweet.id)).volume(1);
+  }, function() {
+    _V_(String(data.tweet.id)).volume(0);
+  });
 
 if ($("#videos").children().length > 8) {
 		$("#videos div:last-child").remove();
