01ef864ced06c0495592c83367fc0f942c2bf63d
Sarah Jane Hong <sarahjane.hong@gmail.com>
2013-02-02 02:36:28 -0500

    set up the server and index page

---
+++ routes/common.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'vinefeed' });
+};
+
+exports.ensureAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+};
