5247ce9fe9229865cbd386863d90a5ce96b60087
Fudge21 <daryn.lam@gmail.com>
2013-02-02 17:33:20 -0500

    Fixed focusing on videos and removed 8 limit video.

--- public/js/client.js
+++ public/js/client.js
@@ -1,13 +1,16 @@
 var socket = io.connect('http://localhost');
 socket.on('tweet', function (data) {
-	$("<video id='" + data.tweet.id + "' class='video-js vjs-default-skin magnify' loop preload='auto' width='200' height='200' src='" + 
-    data.tweet.vid_url + "''></video>").prependTo("#videos");
-    //{"width": "", "height" : ""}
+	$("<div>").append(
+		$("<video id='" + data.tweet.id + "' class='video-js vjs-default-skin magnify' loop preload='auto' width='200' height='200' src='" + 
+    	data.tweet.vid_url + "''></video>")
+	).fadeIn("slow").css("display","inline-block").prependTo("#videos");
 
   _V_(String(data.tweet.id)).ready(function() {
     this.volume(0);
     $("#" + String(data.tweet.id)).css("display", "inline-block");
   });
+
+  $("#" + String(data.tweet.id)).css({"width": "", "height" : ""});
   
   // mouseover in mouseover out callbacks
   $("#" + data.tweet.id).hover(function(){
@@ -19,9 +22,9 @@ socket.on('tweet', function (data) {
     _V_(String(data.tweet.id)).pause();
   });
 
-if ($("#videos").children().length > 8) {
-		$("#videos div:last-child").remove();
-	}
+// if ($("#videos").children().length > 8) {
+// 		$("#videos div:last-child").remove();
+// 	}
 });
 
 /* Updates videos based on search params */
