eb60fa22a773837f5f98cc032df90eb89d9d19ee
Fudge21 <daryn.lam@gmail.com>
2013-02-02 21:39:16 -0500

    Made changes to front-end, all videos aligned and z-indexed.

--- public/js/client.js
+++ public/js/client.js
@@ -10,15 +10,21 @@ socket.on('connect', function() {
   socket.emit('track', { track: 'vine co v'});
 });
 
+var i = 0;
+var list = ["", "300px", "515px", "730px"];
+
 socket.on('tweet', function (data) {
-	$("<div>").append(
+	if (Math.floor(i % 4) == 0) {
+		$("<div id='row" + Math.floor(i/4) + "' class='row' style='margin-left: 0px;'>").appendTo("#videos");
+	}
+	$("<div class='item' style='left: " + list[Math.floor(i% 4)] + "'>").append(
 		$("<video id='" + data.tweet.id + "' class='video-js vjs-default-skin magnify' loop preload='auto' width='200' height='200' src='" + 
     	data.tweet.vid_url + "''></video>")
-	).fadeIn("slow").css("display","inline-block").prependTo("#videos");
+	).fadeIn("slow").css("display","inline-block").appendTo("#row" + Math.floor(i/4));
+	i++;
 
   _V_(String(data.tweet.id)).ready(function() {
     this.volume(0);
-    this.play();
   });
 
   $("#" + String(data.tweet.id)).css({"width": "", "height" : ""});
@@ -26,8 +32,12 @@ socket.on('tweet', function (data) {
   // mouseover in mouseover out callbacks
   $("#" + data.tweet.id).hover(function(){
     _V_(String(data.tweet.id)).volume(1);
+    _V_(String(data.tweet.id)).play();
+    $("#" + data.tweet.id).parent().css("z-index", "2");
   }, function() {
     _V_(String(data.tweet.id)).volume(0);
+    _V_(String(data.tweet.id)).pause();
+    $("#" + data.tweet.id).parent().css("z-index", "1");
   });
 
 // if ($("#videos").children().length > 8) {
--- server.js
+++ server.js
@@ -54,14 +54,14 @@ httpServer.listen(app.get('port'), function() {
 
 var io = socketio.listen(httpServer);
 var twit = new Twit({
-    // consumer_key:         'gA11sspNJdtdCL2gWDQqFA', 
-    // consumer_secret:      'uhBxdHaryjLcAcC9D985WYNbyT9LAzK03FMDbfnBZfc',
-    // access_token:         '150977185-F3trFzvsc3qjFd8DlrMbkWJXjj97IiHkifvP4EjR', 
-    // access_token_secret:  'GOA66sBv471fk8HPYSwomCsarmeO17FYg0Fa6Ao9E'
-    consumer_key:         '4McGP8uCDmQUsIMIPxrIBQ', 
-    consumer_secret:      'uuGskJNWtcUtFQ1Axll41jRhfmUM1dPixBiLIcnMjA',
-    access_token:         '436379768-WBGlOaKu7buJcjDyOECoguuD4dRt4QwI10CPoROP', 
-    access_token_secret:  'p8TnnMSu0R95BqduDmvXvD9LIajUSAdb0stblauei0'
+    consumer_key:         'gA11sspNJdtdCL2gWDQqFA', 
+    consumer_secret:      'uhBxdHaryjLcAcC9D985WYNbyT9LAzK03FMDbfnBZfc',
+    access_token:         '150977185-F3trFzvsc3qjFd8DlrMbkWJXjj97IiHkifvP4EjR', 
+    access_token_secret:  'GOA66sBv471fk8HPYSwomCsarmeO17FYg0Fa6Ao9E'
+    // consumer_key:         '4McGP8uCDmQUsIMIPxrIBQ', 
+    // consumer_secret:      'uuGskJNWtcUtFQ1Axll41jRhfmUM1dPixBiLIcnMjA',
+    // access_token:         '436379768-WBGlOaKu7buJcjDyOECoguuD4dRt4QwI10CPoROP', 
+    // access_token_secret:  'p8TnnMSu0R95BqduDmvXvD9LIajUSAdb0stblauei0'
 })
 
 io.sockets.on('connection', function (socket) {
