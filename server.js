c6078556135a10b9df105bd23fbcc0e8ad8cbcff
Jeff Ling <jeff.ummu@gmail.com>
2013-02-04 01:14:28 -0500

    changed port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 // app.locals = require(' ./locals');
 
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 8080);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
